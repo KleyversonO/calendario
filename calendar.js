a99c2fbb103e67bc1f183f1a826c14da9eaa9cbd
KleyversonO <kleyversonoliveira@gmail.com>
2024-04-05 08:06:17 -0300

    Aprimoramente do style ao código

--- calendar.js
+++ calendar.js
@@ -1,8 +1,28 @@
-function colorirDia(){
+function colorirDia() {
     let days = document.getElementById('day').value;
     let color = document.getElementById('color').value;
     let calendar = document.getElementById('calendar');
-    let td = calendar.getElementsByTagName('td')
-    [(parseInt (days)+2)];
-    td.style.backgroundColor = color;
-}
+    let td = calendar.getElementsByTagName('td')[parseInt(days) + 2];
+     
+    if (days <= 0 || days > 31) {
+        alert("é necessario que digite um número entre 1 e 31.");
+        return;
+      }
+     
+      days = Math.max(1, parseInt(days));
+     
+      let colorCounts = {};
+      if (localStorage.getItem('colorCounts')) {
+        colorCounts = JSON.parse(localStorage.getItem('colorCounts'));
+      }
+     
+      if (colorCounts[color] && colorCounts[color] >= 3) {
+        alert(`A cor "${color}" já foi usada 3 vezes. Escolha outra cor.`);
+        return;
+      }
+     
+      colorCounts[color] = (colorCounts[color] || 0) + 1;
+      localStorage.setItem('colorCounts', JSON.stringify(colorCounts));
+     
+      td.style.backgroundColor = color;
+    }
